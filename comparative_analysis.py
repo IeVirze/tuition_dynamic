75d29b8ea7d37bd2c704684067c923b1cc9961ac
IeVirze <136879608+IeVirze@users.noreply.github.com>
2024-05-06 22:07:52 +0300

    Data and plan update
    Added downloaded .csv files from the from the stats.gov.com about average salary by age groups and median and average salary 2019-2024; Added approximate description what needs to be calculated for a proper analysis and charting

---
+++ comparative_analysis.py
@@ -0,0 +1,10 @@
+#Get additional salary data - earnings by education level - do majority of the people have earnings level that outweight the tuition cost for the degree? 
+#For the most objective we can take also specific averages: 
+    #assume the person started bachelor in PLK in 2019/2020 study year and bachelor was 4 years long
+    #assume that after bachelor a masters degree was aquired (start PLK 2023/2024; finish 2024/2025)
+    #calculate the aproximate salary increase due to inflation 
+    #does the person can afford to pay out-of-pocket for master's studies with the average bachelor's degree salary accounting for living expenses?
+    #does the total cost of studies can be justified on average? 
+    #assume that half of the study period was paid by the government - how does that look like now? 
+    #assume that all bachelor and masters were funded by government - how does that look like
+    #calculate the same things for NLK or NLN student - there is no scholarship or government funded positions - how does the numbers look like now?
---
+++ tuition_calculations.py
@@ -0,0 +1,11 @@
+#need to add a calculation for tuition tables: 
+    #the average and median per degree level (college, bachelore, masters, phd) all programes per year
+    #the average increase in tuition fees against last year (%) and difference
+    #the average calculation for the cost of full degree based on the minimum period of study required and study form: 
+        #college: 2 years PLK
+        #bachelore: 3-4 years PLK
+        #masters: 2-3 years PLK
+        #doctors: 4 years PLK
+    #the average monthly payment if the tuition is covered by the student's finances not loan
+    #how does the average monthly payment compare to the age group's for the specific degree average calculated cost of living and earned salary?
+
